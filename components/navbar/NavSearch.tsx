3e9ea45ad37e581fc898310ff14b45fc2e995680
김태관 <iamtk@neowiz.com>
2024-08-16 20:26:31 +0900

    dark mode

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,4 +1,12 @@
+import { Input } from "../ui/input";
+
 const NavSearch = () => {
-  return <div>NavSearch</div>;
+  return (
+    <Input
+      className="max-w-xs dark:bg-muted"
+      type="text"
+      placeholder="find a property..."
+    />
+  );
 };
 export default NavSearch;
