1103ac880ffa027342f649e79e59c1128cc38ada
김태관 <iamtk@neowiz.com>
2024-09-02 18:56:31 +0900

    BreadCrumb

---
+++ app/properties/[id]/page.tsx
@@ -0,0 +1,23 @@
+import FavoriteToggleButton from "@/components/card/FavoriteToggleButton";
+import BreadCrumbs from "@/components/properties/BreadCrumbs";
+import { fetchPropertyDetails } from "@/utils/actions";
+import { redirect } from "next/navigation";
+
+const PropertyDetailsPage = async ({ params }: { params: { id: string } }) => {
+  const property = await fetchPropertyDetails(params.id);
+  if (!property) redirect("/");
+  const { baths, bedrooms, beds, guests } = property;
+  const detail = { baths, bedrooms, beds, guests };
+  return (
+    <section>
+      <BreadCrumbs name={property.name} />
+      <header className="flex justify-between items-center mt-4">
+        <h1 className="text-4xl font-bold capitalize">{property.tagline}</h1>
+        <div>
+          <FavoriteToggleButton propertyId={property.id} />
+        </div>
+      </header>
+    </section>
+  );
+};
+export default PropertyDetailsPage;
