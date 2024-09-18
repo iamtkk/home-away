dcd8e22f88ab03739ada399ca015e6f131e8fdd1
김태관 <iamtk@neowiz.com>
2024-09-18 14:08:28 +0900

    review delete

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,4 +1,26 @@
-const Comment = () => {
-  return <div>Comment</div>;
+"use client";
+import { useState } from "react";
+import { Button } from "../ui/button";
+
+const Comment = ({ comment }: { comment: string }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpand = () => setIsExpanded((prev) => !prev);
+  const longComment = comment.length > 130;
+  const displayComment =
+    longComment && !isExpanded ? `${comment.slice(0, 130)}...` : comment;
+  return (
+    <div>
+      <p className="text-sm">{displayComment}</p>
+      {longComment && (
+        <Button
+          className="pl-0 text-muted-foreground"
+          onClick={toggleExpand}
+          variant="link"
+        >
+          {isExpanded ? "Read less" : "Read more"}
+        </Button>
+      )}
+    </div>
+  );
 };
 export default Comment;
